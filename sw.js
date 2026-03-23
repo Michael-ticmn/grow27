3cd065951d05465dc3453e9020fc66ab83109681
Michael <Michael@ticmn.com>
2026-03-23 09:57:49 -0500

    v1.52 - your commit message here

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'grow27-v1.51';
+const CACHE = 'grow27-v1.52';
 
 // Core files to precache for offline use
 const PRECACHE = [
@@ -104,6 +104,7 @@ self.addEventListener('fetch', e => {
     })
   );
 });
+
 
 
 
